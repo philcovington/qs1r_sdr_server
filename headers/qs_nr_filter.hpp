d2749c78ff6ce524808ae1c78cc0b0505a100e66
Philip A Covington <p.covington@gmail.com>
2024-10-17 14:13:26 -0700

    added description header

--- headers/qs_nr_filter.hpp
+++ headers/qs_nr_filter.hpp
@@ -1,3 +1,30 @@
+/**
+ * @file    qs_noise_reduction_filter.h
+ * @brief   Noise Reduction Filter for signal processing.
+ * 
+ * This class implements a noise reduction filter designed to enhance the 
+ * quality of signals by reducing unwanted noise components. The filter 
+ * utilizes an adaptive algorithm to dynamically adjust its coefficients 
+ * based on the incoming signal characteristics, allowing for effective 
+ * noise suppression while preserving the integrity of the desired signal.
+ * 
+ * Features:
+ * - Adaptive noise reduction based on the signal content.
+ * - Configurable adaptation rate and leakage for performance tuning.
+ * - Flexible delay handling for varied signal processing needs.
+ * 
+ * Usage:
+ * - Create an instance of the class and call init() to set the desired 
+ *   size.
+ * - Use process() to apply the noise reduction to a vector of complex signals.
+ * 
+ * @note This class is intended for use in digital signal processing (DSP) 
+ *       applications requiring effective noise mitigation.
+ * 
+ * @author  Philip A Covington
+ * @date    2024-10-17
+ */
+
 #pragma once
 
 #include "../headers/qs_dataproc.h"
