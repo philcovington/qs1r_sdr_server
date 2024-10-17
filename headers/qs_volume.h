839f2c86faf3d7d852ebee6e84d0e77cdf7b5325
Philip A Covington <p.covington@gmail.com>
2024-10-17 11:01:09 -0700

    added volume control

---
+++ headers/qs_volume.h
@@ -0,0 +1,45 @@
+/*
+ * QsVolume.h
+ * 
+ * Overview:
+ * This class provides functionality to adjust the amplitude (or volume) of complex signals
+ * and floating-point signals. It allows for dynamic volume control in decibels.
+ * 
+ * Features:
+ * - Adjusts amplitude of complex signals represented as `qs_vect_cpx`.
+ * - Adjusts amplitude of real-valued signals represented as `qs_vect_f`.
+ * 
+ * Usage:
+ * - Create an instance of `QsVolume` and use the `process` methods to modify the amplitude
+ *   of input signals.
+ * 
+ * Notes:
+ * - Volume adjustments are based on a logarithmic scale (decibels).
+ * - Ensure that the input signals are properly initialized before processing.
+ * 
+ * Author: Philip A Covington
+ * Date: 2024-10-17
+ */
+
+#pragma once
+
+#include "../headers/qs_dataproc.h"
+#include "../headers/qs_globals.h"
+
+using namespace std;
+
+class QsVolume {
+
+  private:
+    float m_volume_db;
+    float m_volume_val;
+
+    qs_vect_cpx::iterator cpx_itr;
+    qs_vect_f::iterator f_itr;
+
+  public:
+    QsVolume();
+
+    void process(qs_vect_cpx &src_dst);
+    void process(qs_vect_f &src_dst);
+};
---
+++ src/qs_volume.cpp
@@ -0,0 +1,19 @@
+#include "../headers/qs_volume.h"
+
+QsVolume ::QsVolume() : m_volume_db(0), m_volume_val(0) {}
+
+void QsVolume ::process(qs_vect_cpx &src_dst) {
+    m_volume_db = QsGlobal::g_memory->getVolume();
+    m_volume_val = pow(10.0, m_volume_db / 20.0);
+    for (cpx_itr = src_dst.begin(); cpx_itr != src_dst.end(); cpx_itr++) {
+        (*cpx_itr) *= m_volume_val;
+    }
+}
+
+void QsVolume ::process(qs_vect_f &src_dst) {
+    m_volume_db = QsGlobal::g_memory->getVolume();
+    m_volume_val = pow(10.0, m_volume_db / 20.0);
+    for (f_itr = src_dst.begin(); f_itr != src_dst.end(); f_itr++) {
+        (*f_itr) *= m_volume_val;
+    }
+}
